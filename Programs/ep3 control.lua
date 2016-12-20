b7e10f889fd469c3a8dbddd195a281591e90493f
MajorGeneralRelativity <majgenrelativity@gmail.com>
2016-12-20 17:00:30 -0500

    Update EP3 Control and remove CAnswer (was dumb)
    EP3 control now supports variable addresses

--- Programs/ep3 control.lua
+++ Programs/ep3 control.lua
@@ -1,5 +1,11 @@
 local component = require("component")
 local dial = component.ep_dialling_device
-dial.dial("3 3 3 3 3 3 3 3 3")
-os.sleep(15)
-dial.terminate()
+local input = ""
+while true do
+    print("Enter number to dial. Must be at 1-9 digit number, with each digit separated by a space. (e.g. 1 2 3 4)")
+    input = io.read
+    dial.dial(tostring(input))
+    os.sleep(15)
+    dial.terminate()
+    os.sleep(1)
+end
