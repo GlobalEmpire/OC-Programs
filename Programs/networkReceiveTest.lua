fd58f773d0f18da32e06c4df8776797669e109be
Gavle <gavleissweden@gmail.com>
2016-12-20 14:25:01 -0500

    Delete networkReceiveTest.lua

--- Programs/networkReceiveTest.lua
+++
@@ -1,8 +0,0 @@
-local fs = require("filesystem")
-local component = require("component")
-local event = require("event")
-local m = component.modem
-m.open(123)
-local _, _, from, port, _, message = event.pull("modem_message")
-print("Got a message from " .. from .. " on port " .. port .. ": " .. tostring(message))
-fs.copy("/data/usr/programs/modemReceiveTest.lua", "/mnt/34d/modemReceiveTest.lua")
