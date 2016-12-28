5899c6440a87a9f0dfa0753079e1708873ed9afc
MajorGeneralRelativity <majgenrelativity@gmail.com>
2016-12-28 14:58:04 -0500

    yargh

--- Programs/TACEATS2 - Installer.lua
+++ Programs/TACEATS2 - Installer.lua
@@ -21,12 +21,9 @@ if filesystem.exists("/usr/programs/TMain.lua") and filesystem.exists("/usr/prog
     local reason2, junk2 = bagel.glutenous("/usr/programs/TFiCo.lua", 261169)
     local reason3, junk3 = bagel.glutenous("/usr/programs/TIdent.lua", 259827)
     if reason1 == true and reason2 == true and reason3 == true then
-        bagel.unToastFile("/usr/programs/TMain.lua", "/tmp/TMain.lua")
-        bagel.unToastFile("/usr/programs/TIdent.lua", "/tmp/TIdent.lua")
-        bagel.unToastFile("/usr/programs/TFiCo.lua", "/tmp/TFiCo.lua")
-        shell.execute("/tmp/TFiCo.lua")
-        shell.execute("/tmp/TIdent.lua")
-        shell.execute("/tmp/TMain.lua")
+        shell.execute("/usr/programs/TFiCo.lua")
+        shell.execute("/usr/programs/TIdent.lua")
+        shell.execute("/usr/programs/TMain.lua")
     else
         print("An altered copy of TACEATS2 is present on this system. Preventing execution to secure against malicious code injection. Please report to whomever you got this program from, and to Gavle on irc.esper.net")
         print("Have a good day!")
@@ -34,9 +31,9 @@ if filesystem.exists("/usr/programs/TMain.lua") and filesystem.exists("/usr/prog
 else
     print("TACEATS files missing, attempting download now")
     if component.isAvailable("internet") == true then
-        AcquirePaste("https://raw.githubusercontent.com/MajorGeneralRelativity/OC-Programs/master/Programs/TACEATS2%20-%20Main.lua", "/usr/programs/TMain.lua")
-        AcquirePaste("https://raw.githubusercontent.com/MajorGeneralRelativity/OC-Programs/master/Programs/TACEATS2%20-%20Target%20Identification.lua", "/usr/programs/TIdent.lua")
-        AcquirePaste("https://raw.githubusercontent.com/MajorGeneralRelativity/OC-Programs/master/Programs/TACEATS2%20-%20FireControl.lua", "/usr/programs/TFiCo.lua")
+        AcquirePaste("https://raw.githubusercontent.com/MajorGeneralRelativity/OC-Programs/master/Programs/TMain.lua", "/usr/programs/TMain.lua")
+        AcquirePaste("https://raw.githubusercontent.com/MajorGeneralRelativity/OC-Programs/master/Programs/TIdent.lua", "/usr/programs/TIdent.lua")
+        AcquirePaste("https://raw.githubusercontent.com/MajorGeneralRelativity/OC-Programs/master/Programs/TFiCo.lua", "/usr/programs/TFiCo.lua")
     else
         print("Internet card not available, please insert one, and then re-run this program")
     end
