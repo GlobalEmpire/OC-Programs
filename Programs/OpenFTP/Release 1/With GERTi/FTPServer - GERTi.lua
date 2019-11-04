dfff9a7298f049f34c6c6a23af73a4edb55eea17
leothehero <leothehero@users.noreply.github.com>
2019-11-04 18:14:32 +0100

    Update FTPServer - GERTi.lua

--- Programs/OpenFTP/Release 1/With GERTi/FTPServer - GERTi.lua
+++ Programs/OpenFTP/Release 1/With GERTi/FTPServer - GERTi.lua
@@ -18,7 +18,6 @@ local socket = {}
 
 local function Register(_, Address, CID)
 	if CID == 98 then 
-		print("Registering")
 		socket[Address] = GERTi.openSocket(Address, true, 98)
 	end
 end
@@ -26,7 +25,6 @@ event.listen("GERTConnectionID", Register)
 
 local function CloseSocket(_, Address, _, CID)
 	if CID == 98 then
-		print("Closing")
 		socket[Address]:close()
 	end
 end
@@ -35,47 +33,38 @@ event.listen("GERTConnectionClose", CloseSocket)
 
 
 local function Receive(Address, Type, Content, Spare)
-	print(Address, Type, Content, Spare)
 	if Type == "S.FileStart" then 
 		local ReceiverAddress = Address
 		filename = math.random(1000000, 9999999)
 		path[ReceiverAddress] = tostring(filename .. "." .. Content)
-		print("Creating file from " .. ReceiverAddress .. " With Address " .. path[ReceiverAddress]) 
 		fileReceive[ReceiverAddress] = io.open(tostring("/home/" .. path[ReceiverAddress]), "w")
 		socket[ReceiverAddress]:write("StartConfirm")
-		print("Confirmed")
 	elseif Type == "S.FileContinue" then
 		local ReceiverAddress = Address
 		fileReceive[ReceiverAddress]:write(Content)
-	--	print("Written")
 		socket[ReceiverAddress]:write("WriteContinue")
 	elseif Type == "S.FileFin" then
 		local ReceiverAddress = Address
 		fileReceive[ReceiverAddress]:close()
-	--	print("Complete")
 		socket[ReceiverAddress]:write(path[ReceiverAddress])
 	elseif Type == "FTPErrorR" then
 		fileReceive[Address]:close()
 	elseif Type == "R.FileStart" then 
-		print("Start Stream")
 		local SenderAddress = Address
 		fileSend[SenderAddress] = io.open(tostring("/home/" .. Content))
 		socket[SenderAddress]:write("R.Ready")
 	elseif Type == "R.FileCont" then
 		local SenderAddress = Address
-		print("Continue Stream")
 		P[SenderAddress] = fileSend[SenderAddress]:read(4096)
 		socket[SenderAddress]:write("R.FTPCont")
 		socket[SenderAddress]:write(P[SenderAddress])
 		os.sleep(1)
 		while string.len(P[SenderAddress]) == 4096 do
-			print("Continue Stream")
 			P[SenderAddress] = fileSend[SenderAddress]:read(4096)
 			socket[SenderAddress]:write("R.FTPCont")
 			socket[SenderAddress]:write(P[SenderAddress])
 			os.sleep(1)
 		end
-		print("Stream Finished")
 		socket[SenderAddress]:write("R.Fin")
 		socket[SenderAddress]:write(0)
 		fileSend[SenderAddress]:close()
@@ -83,31 +72,16 @@ local function Receive(Address, Type, Content, Spare)
 end
 
 local function DataGroup(_, Address, CID, data)
-	print(socket)
-	print("DataGroup Called")
-	print(Address)
-	print(CID)
-	print(data)
 	if CID == -1 then
 		if data == "GetVersion" then
 			GERTi.send(Address, Compatibility)
-			print("Standalone message recieved - typeVersion")
 		end
 	elseif CID == 98 then
-		print("recording")
-		print(1)
 		local buffer = socket[Address]:read()
-		print(2)
-		print(buffer[1])
-		print(3)
-		print(messageBuffer1[Address], messageBuffer2[Address], messageBuffer3[Address], buffer[1]) 
 		if messageBuffer1[Address] == nil then messageBuffer1[Address] = buffer[1]
 		elseif messageBuffer2[Address] == nil then messageBuffer2[Address] = buffer[1]
 		else messageBuffer3[Address] = buffer[1]
-		print(messageBuffer1[Address], messageBuffer2[Address], messageBuffer3[Address], buffer[1]) 
-		print("Batching")
 		Receive(Address, messageBuffer1[Address], messageBuffer2[Address], messageBuffer3[Address])
-		print("Clearing")
 		messageBuffer1[Address], messageBuffer2[Address], messageBuffer3[Address] = nil, nil, nil
 		end
 	end
@@ -117,7 +91,6 @@ event.listen("GERTData", DataGroup)
 print("Server Initialised")
 while true do
 local usrstate = io.read()
-if usrstate == "hide" or "Hide" then os.exit() elseif usrstate == "ADDRESS" then print(print(GERTi.getAddress())) 
-end
+if usrstate == "hide" or "Hide" then os.exit() elseif usrstate == "ADDRESS" then print(GERTi.getAddress()) end
 
 end
