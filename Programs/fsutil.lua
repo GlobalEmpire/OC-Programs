e5cfe1ddf80c133de15a402767f3908650bb7b0c
Ocawesome101 <50844998+Ocawesome101@users.noreply.github.com>
2020-02-20 21:20:19 -0500

    it works now

--- Programs/fsutil.lua
+++ Programs/fsutil.lua
@@ -1,114 +1,101 @@
--- Manage filesystems according the the specifications defined at https://github.com/GlobalEmpire/OpenStandards/blob/master/Filesystems/OpenUPT.md and OpenFS.md
--- Currently Open Kernel only
+-- OpenUPT (https://github.com/GlobalEmpire/OpenStandards/blob/master/Filesystems/OpenUPT.md) parsing and generally make-life-easier library. --
 
-local args = {...}
+local openupt = {}
 
-local openupt = require("openupt")
-
-if #args < 1 or args[1] == "help" then
-  error([[FSUtil (c) 2020 Ocawesome101. Licensed under the MIT license.
-Usage: fsutil OPERATION ...
-FSUtil will attempt to auto-detect unmanaged drives.
-Available OPERATIONs are:
- format [mode=secure|fast]:           Format a disk with an OpenUPT partition table. If SECURE is specified, will zero the entire disk.
- mkpart START END LABEL:  Add a partition from sector START to sector END.
- delpart:                 Delete a partition.
- lspart:                  List the label, type, and GUID of all partitions on a drive.
-]])
+if _VERSION == "Lua 5.2" then
+  error("Lua 5.2 is unsupported. Please use Lua 5.3 instead.")
   return false
 end
 
--- Default bootsector.
-local boot_sector = [[local gpu, screen = component.list("gpu")(), component.list("screen")()
-if gpu and screen then
-  component.invoke(gpu, "bind", screen)
-  component.invoke(gpu, "set", 1, 1, "Non-system disk or disk error. Press any key to reboot.")
-  repeat
-    local e = computer.pullSignal()
-  until e == "key_down"
-  computer.shutdown(true)
-end
-]]
+local ptable = setmetatable({}, { __index = table })
 
-print("Detecting unmanaged drives....")
-local drives = {}
-local drive = ""
-for addr, ctype in component.list("drive") do
-  if ctype == "drive" then
-    table.insert(drives, addr)
+local zero = string.char(0)
+
+local function r(n, n2)
+  return tostring(string.char(math.random(n,n2)))
+end
+
+local function randomGUID()
+  local g = table.concat({
+    r(0,255), r(0,255), r(0,255), r(0,255),
+    r(0,255), r(0,255), r(0,255), r(0,255)
+  }, "")
+  return g
+end
+
+function openupt.setPartitionTable(tableData)
+  while #ptable > 0 do
+    ptable:remove(1)
   end
-end
-
-if #drives == 0 then
-  return error("No unmanaged drives found")
-end
-
-if #drives == 1 then
-  drive = component.proxy(drives[1])
-end
-
-if #drives > 1 then
-  print("Choose a drive:")
-  for i=1, #drives, 1 do
-    print(tostring(i) .. ". " .. drives[i]:sub(1, 6))
-  end
-  local c
-  repeat
-    local e, _, id = event.pull()
-    if e == "key_down" then c = tonumber(string.char(id)) end
-  until c <= #drives
-  drive = component.proxy(drives[c])
-end
-
-if args[1] == "format" then
-  if args[2] and args[2] ~= "mode=secure" and args[2] ~= "mode=fast" then
-    error("Unrecognized option " .. args[2])
-    return false
-  end
-  print("WARNING: Formatting a disk will erase all currently stored data!")
-  local inp = ""
-  repeat
-    write("Are you sure you want to continue? [y/N]: ")
-    local e, id
-    repeat
-      e, _, id = event.pull()
-    until e == "key_down"
-    if e == "key_down" then
-      inp = string.char(id)
-      if inp:lower() == "n" then
-        print("Operation canceled. Have a nice day.")
-        return
-      end
+  local p = setmetatable({}, { __index = table })
+  for i=1, 512, 64 do
+    local pstart, pend, fstype, pflags, guid, reserved, label = string.unpack("<IIc8Ic8Ic32", tableData:sub(i, i + 64))
+    if guid and guid ~= "" and guid:sub(1, 1) ~= " " and string.byte(guid:sub(1, 1)) ~= 0 then
+      ptable:insert({pstart, pend, fstype, pflags, guid, reserved, label})
     end
-    write("\n")
-  until inp:lower() == "y"
-  print("Formatting " .. drive.address:sub(1, 6))
-  local zero = string.char(0):rep(512)
-  if args[2] and args[2] == "mode=secure" then
-    write("Zeroing drive. This might take a while...")
-    for i=1, drive.getCapacity() / 512, 1 do
-      if i % 8 == 0 then
-        write(".")
-      end
-      drive.writeSector(i, zero)
-    end
-    print("done")
   end
-  write("Writing boot sector...")
-  drive.writeSector(1, boot_sector)
-  print("done.")
-elseif args[1] == "mkpart" then
-  if not args[2] and args[3] then
-    return error("Missing arguments. Run 'fsutil help' for help.")
-  end
-  print("Reading existing partition table")
-  local ptable = drive.readSector(25)
-  openupt.setPartitionTable(ptable)
-  if openupt.partitionCount() == 8 then
-    return error("The maximum supported number of partitions on volume " .. drive.address:sub(1,6) .. " has been reached.")
-  end
-  print("Creating partition")
-  openupt.mkpart(args[2], args[3], args[4])
-  drive.writeSector(25, openupt.rawPartitionTable())
-  print("done.")
 end
+
+function openupt.partitions() -- Return some info about partitions
+  local rtn = {}
+  for i=1, #ptable, 1 do
+    table.insert(rtn, {guid = ptable[i][5], fstype = ptable[i][3], size = (ptable[i][1] + ptable[i][2]) * 512, label = ptable[i][7]})
+  end
+  return rtn
+end
+
+function openupt.partitionCount()
+  return #openupt.partitions()
+end
+
+function openupt.mkpart(pstart, pend, ptype, flags, label)
+  checkArg(1, pstart, "number")
+  checkArg(2, pend, "number")
+  checkArg(3, ptype, "string", "nil")
+  checkArg(4, flags, "number", "nil")
+  checkArg(5, label, "string", "nil")
+  local ptype, label = ptype or zero:rep(8), label or zero:rep(32)
+  local ptype = ptype:sub(1, 8)
+  local flags = flags or 0x00000001
+  local ptype = ptype or zero:rep(1, 8)
+  while #ptype < 8 do
+    ptype = ptype .. zero
+  end
+  while #label < 32 do
+    label = label .. zero
+  end
+  if #ptable == 8 then
+    return error("Too many partitions")
+  end
+  if pstart < 33 then
+    pstart = 33
+  end
+  -- TODO: Flag parsing to get them in the right format (maybe?)
+  -- also, it's currently up to the wrapper utility to verify that sectors are available. It would probably be better to have all partition management done in openUPT itself.
+  ptable:insert({pstart, pend, ptype, flags, randomGUID(), 0x00000000, label})
+end
+
+function openupt.delpart(guid) -- Delete a partition based on its GUID
+  checkArg(1, guid, "string")
+  for i=1, #ptable, 1 do
+    if ptable[i][5] == guid then
+      table.remove(ptable, i)
+      return true
+    end
+  end
+  return false
+end
+
+function openupt.rawPartitionTable()
+  local r = ""
+  for i=1, #ptable, 1 do
+    r = r .. string.pack("<IIc8Ic8Ic32", table.unpack(ptable[i]))
+  end
+  local z = string.char(0)
+  while #r < 512 do
+    r = r .. z
+  end
+  return r
+end 
+
+return openupt
