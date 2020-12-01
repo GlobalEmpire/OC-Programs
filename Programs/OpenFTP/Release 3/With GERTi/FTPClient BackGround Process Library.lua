f1072072a3dcbce92c255bbe1688a78b9aaf6ba8
leothehero <leothehero@users.noreply.github.com>
2020-12-01 23:59:34 +0100

    Update FTPClient BackGround Process Library.lua

--- Programs/OpenFTP/Release 3/With GERTi/FTPClient BackGround Process Library.lua
+++ Programs/OpenFTP/Release 3/With GERTi/FTPClient BackGround Process Library.lua
@@ -5,26 +5,55 @@ local event = require("event")
 local m = component.modem
 local GERTi = require("GERTiClient")
 local DC = component.data
+--if DC.generateKeyPair == nil then print("This service requires a T3 data card to be installed. There is no T3 data card detected by this program. Please ensure that you have a ///T3/// datacard installed, or install the no-encryption version of this program if it exists.") return end
 local shell = require("shell")
 local args, opts = shell.parse(...)
 local fs = request("filesystem")
 local serialization = require("serialization")
+--Program Variables:
+local ConfigSettings = []
+local SendData = []
+
+
+
+--Program Error Codes:
+local FILENOTFOUND = 0
+local INVALIDCREDENTIALS = 1
+local INVALIDFILELOCATION = 2
+local INVALIDSERVERADDRESS = 3
+local NILSERVERADDRESS = 4
+local INCOMPATIBLESERVER = 5
+local CONFIGDIRECTORYISFILE = 10
+
+
+--OneTimeRun Code:
+
 if fs.isDirectory(".config") then
     if fs.exists(".config/.OFTPLIB") then
         local ConfigFile = io.open(".config/.OFTPLIB")
         ConfigSettings = serialization.unserialize(ConfigSettings:read())
     end
 end
---Program Error Codes:
-local FILENOTFOUND = 0
-local INVALIDCREDENTIALS = 1
-local INVALIDFILELOCATION = 2
-local CONFIGDIRECTORYISFILE = 10
 
 --Private Functions:
 local function VerifyServer(address,compatibility) -- Verify that the server exists and has a sufficient compatibility level
-    --Verify Server exists
-    --Verify Compatibility
+    if address then
+        --Verify Server exists:
+        GERTi.send(FTPaddress, "GetVersion")
+        local _, _, _, ServerVersion = event.pull(15, "GERTData") --This is a sub-optimal implementation, as it triggers on the first received message, and ignores future messages. This could/will be bad on computers that directly receive high traffic through GERT. When possible, implement a system that checks that it was a response from the server you asked.
+        --Verify Compatibility:
+        if ServerVersion then 
+            if ServerVersion >= compatibility then
+                return true, 0
+            else
+                return false, INVALIDSERVERADDRESS
+            end
+        else
+            return false, INCOMPATIBLESERVER
+        end
+    else
+        return false, NILSERVERADDRESS
+    end
 end
 
 local function VerifyCredentials(User,Password)
@@ -32,12 +61,18 @@ local function VerifyCredentials(User,Password)
 end
 
 --Public Functions
-function RequestPackage(PackageName,GivenServer) -- This function is for requesting packages from the set FTP server.
+function RequestPackage(PackageName,GivenServer) -- This function is for requesting packages from the set FTP server. Packages are always public.
     GivenServer = GivenServer or ConfigSettings["DefaultServer"]
     if PackageName then
-        if VerifyServer(GivenServer, Compatibility) then
-            --Open Server Connection
-            --Request Package from server
+        VerSer, code = VerifyServer(GivenServer, Compatibility)
+        if VerSer then
+            local FTPSocket = GERTi.openSocket(GivenServer, true, 98) --Open Server Connection
+            local CID = 0 --Wait for server to open back
+            while CID ~= 98 do
+                _, _, CID = event.pull("GERTConnectionID")
+            end
+            SendData["Mode"] = "RequestPackage"
+            SendData["Name"] = tostring(PackageName)
             if ServerResponse then
                 --Receive Package
                 --Close server connection
@@ -45,6 +80,9 @@ function RequestPackage(PackageName,GivenServer) -- This function is for request
             else
                 return false, FILENOTFOUND
             end
+        else
+            return VerSer, code
+        
         end
     end
 end
@@ -79,18 +117,22 @@ function SendFile(FileName,GivenServer,User,Password)
 
 end
 
---Make a args section with: 
---[1] = "setup", [2] = server address
+--For manual execution, or .shrc
 if args[1] == "setup" then
     ConfigSettings["DefaultServer"] = args[2]
     if fs.exists(".config") then
         if fs.isDirectory(".config") then
             local ConfigFile = io.open(".config/.OFTPLIB", "w")
             ConfigFile:write(serialization.serialize(ConfigSettings))
+            ConfigFile:close()
         else
             print(false)
             print(CONFIGDIRECTORYISFILE)
         end
+    else
+        fs.makeDirectory(".config")
+        local ConfigFile = io.open(".config/.OFTPLIB", "w")
+        ConfigFile:write(serialization.serialize(ConfigSettings))
+        ConfigFile:close()
     end
-
 end
