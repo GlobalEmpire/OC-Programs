0cc90466fc7bd4fa3a9f52a8678567c4c077ee80
leothehero <leothehero@users.noreply.github.com>
2021-03-26 08:57:08 +0100

    Update AutoUpdateGERTiMNC.lua

--- Programs/AutoUpdaters/Tonatsi/AutoUpdateGERTiMNC.lua
+++ Programs/AutoUpdaters/Tonatsi/AutoUpdateGERTiMNC.lua
@@ -3,14 +3,15 @@ if component.filesystem.isReadOnly() then
 end
 local fs = require("filesystem")
 if not(component.isAvailable("internet")) then
-    if not(fs.exists("../GERTiMNCautoUpdateError")) then
-        local errorfile = io.open("../GERTiMNCautoUpdateError", "w")
+    if not(fs.exists("/GERTiMNCautoUpdateError")) then
+        local errorfile = io.open("/GERTiMNCautoUpdateError", "w")
         errorfile:write("No Internet Card Found, aborting auto update process until this file is deleted.")
         errorfile:close() 
     end
 end
-if not(fs.exists("../GERTiMNCautoUpdateError")) then    
+if not(fs.exists("/GERTiMNCautoUpdateError")) then    
     local internet = require("internet")
+    if fs.exists("/etc/rc.d/GERTiMNC.lua")
     local GERTiMNCversionRequest = internet.request("https://raw.githubusercontent.com/GlobalEmpire/GERT/master/GERTi/GERTiClient.lua")
     local Request = internet.request("https://raw.githubusercontent.com/GlobalEmpire/GERT/master/GERTi/GERTiClient.lua")
     local testfile = io.open("/home/testfile.lua", "w")
