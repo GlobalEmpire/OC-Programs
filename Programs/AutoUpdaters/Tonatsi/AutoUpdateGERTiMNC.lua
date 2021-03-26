8b575bbb2d4f922906122781f22dd9dc8f23cf83
leothehero <leothehero@users.noreply.github.com>
2021-03-26 09:07:31 +0100

    Finished the auto updater (no bugtesting yet)

--- Programs/AutoUpdaters/Tonatsi/AutoUpdateGERTiMNC.lua
+++ Programs/AutoUpdaters/Tonatsi/AutoUpdateGERTiMNC.lua
@@ -11,12 +11,19 @@ if not(component.isAvailable("internet")) then
 end
 if not(fs.exists("/GERTiMNCautoUpdateError")) then    
     local internet = require("internet")
-    if fs.exists("/etc/rc.d/GERTiMNC.lua")
-    local GERTiMNCversionRequest = internet.request("https://raw.githubusercontent.com/GlobalEmpire/GERT/master/GERTi/GERTiClient.lua")
-    local Request = internet.request("https://raw.githubusercontent.com/GlobalEmpire/GERT/master/GERTi/GERTiClient.lua")
-    local testfile = io.open("/home/testfile.lua", "w")
-    for element in Request do
-        testfile:write(element)
+    if fs.exists("/etc/rc.d/GERTiMNC.lua") then
+        local GERTiMNCVersionRequest = internet.request("https://raw.githubusercontent.com/leothehero/OC-Programs/AutoUpdaters/Programs/AutoUpdaters/Tonatsi/GERTiMNC-latestStableVersion.txt")
+        local GERTiMNCLatestVersion = GERTiMNCVersionRequest()
+        local GERTiMNC = io.open("/etc/rc.d/GERTiMNC.lua", "r")
+        local GERTiMNCVersion = GERTiMNC:read("*l")
+        if GERTiMNCVersion == GERTiMNCLatestVersion then
+            return
+        end
     end
-    testfile:close()
+    local GERTiMNCRequest = internet.request("https://raw.githubusercontent.com/GlobalEmpire/GERT/master/GERTi/GERTiMNC.lua")
+    local GERTiMNC = io.open("/etc/rc.d/GERTiMNC.lua", "w")
+    for element in GERTiMNCRequest do
+        GERTiMNC:write(element)
+    end
+    GERTiMNC:close()
 end
