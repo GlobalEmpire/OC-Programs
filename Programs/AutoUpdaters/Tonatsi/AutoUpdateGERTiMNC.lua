c7d1cd70e20ffc19c621214fd69b8a74bfe384f8
leothehero <leothehero@users.noreply.github.com>
2021-03-26 10:58:08 +0100

    Improvements
    Added a readme, auto updater for GERTiClient, version file for GERTiClient, and added periodic update checking for both, as well as a small 15-second-periodic double beep to signify that the computer needs to be restarted (GERTiClient only)

--- Programs/AutoUpdaters/Tonatsi/AutoUpdateGERTiMNC.lua
+++ Programs/AutoUpdaters/Tonatsi/AutoUpdateGERTiMNC.lua
@@ -10,27 +10,31 @@ if not(component.isAvailable("internet")) then
         errorfile:close() 
     end
 end
-if not(fs.exists("/GERTiMNCautoUpdateError")) then    
-    local internet = require("internet")
-    local event = require("event")
-    if fs.exists("/etc/rc.d/GERTiMNC.lua") then
-        local GERTiMNCVersionRequest = internet.request("https://raw.githubusercontent.com/leothehero/OC-Programs/AutoUpdaters/Programs/AutoUpdaters/Tonatsi/GERTiMNC-latestStableVersion.txt")
-        local GERTiMNCLatestVersion = GERTiMNCVersionRequest()
-        local GERTiMNC = io.open("/etc/rc.d/GERTiMNC.lua", "r")
-        local GERTiMNCVersion = GERTiMNC:read("*l")
-        if GERTiMNCVersion == GERTiMNCLatestVersion then
-            return
+local event = require("event")
+local function update()
+    if not(fs.exists("/GERTiMNCautoUpdateError")) then    
+        local internet = require("internet")
+        if fs.exists("/etc/rc.d/GERTiMNC.lua") then
+            local GERTiMNCVersionRequest = internet.request("https://raw.githubusercontent.com/leothehero/OC-Programs/AutoUpdaters/Programs/AutoUpdaters/Tonatsi/GERTiMNC-latestStableVersion.txt")
+            local GERTiMNCLatestVersion = GERTiMNCVersionRequest()
+            local GERTiMNC = io.open("/etc/rc.d/GERTiMNC.lua", "r")
+            local GERTiMNCVersion = GERTiMNC:read("*l")
+            if GERTiMNCVersion == GERTiMNCLatestVersion then
+                return
+            end
         end
+        local GERTiMNCRequest = internet.request("https://raw.githubusercontent.com/GlobalEmpire/GERT/master/GERTi/GERTiMNC.lua")
+        local GERTiMNC = io.open("/etc/rc.d/GERTiMNC.lua", "w")
+        for element in GERTiMNCRequest do
+            GERTiMNC:write(element)
+        end
+        GERTiMNC:close()
+        local listener = io.open("/.GERTiMNCShutdownListener", "r")
+        local ID = tonumber(listener:read())
+        listener:close()
+        event.cancel(ID)
+        computer.shutdown(true)
     end
-    local GERTiMNCRequest = internet.request("https://raw.githubusercontent.com/GlobalEmpire/GERT/master/GERTi/GERTiMNC.lua")
-    local GERTiMNC = io.open("/etc/rc.d/GERTiMNC.lua", "w")
-    for element in GERTiMNCRequest do
-        GERTiMNC:write(element)
-    end
-    GERTiMNC:close()
-    local listener = io.open("/.GERTiMNCShutdownListener", "r")
-    local ID = tonumber(listener:read())
-    listener:close()
-    event.cancel(ID)
-    computer.shutdown(true)
 end
+update()
+event.timer(900, update, math.huge)
