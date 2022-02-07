a4de12f88760c8ad9deb5f923e359462dca7d266
leothehero <leothehero@users.noreply.github.com>
2022-02-07 16:43:14 +0100

    Update OpenFTP Server Program LITE.lua

--- Programs/OpenFTP/Beta/Beta 3/OpenFTP Server Program LITE.lua
+++ Programs/OpenFTP/Beta/Beta 3/OpenFTP Server Program LITE.lua
@@ -3,6 +3,7 @@
 local fs = require("filesystem")
 local GERTi = require("GERTiClient")
 local event = require("event")
+local SRL = require("serialization")
 
 local FTPCore = require("FTPCore")
 
@@ -42,6 +43,10 @@ local function GERTDataHandler(_,originAddress,connectionID,data)
                     port = customPort
                 }
                 local result, lastState = FTPCore.UploadFile(FileDetails,true,fileSockets[originAddress]) -- I might do something with these outputs later
+            elseif information[1] == "FTPSENDPROBE" then
+                local FileDetails = SRL.unserialize(information[2])
+                local FileData = SRL.unserialize(information[3])
+                local result, lastState = FTPCore.DownloadFile(FileDetails,FileData,fileSockets[originAddress])
             end
         end
     end
@@ -55,10 +60,13 @@ local completeSocketListener = event.listen("GERTConnectionID",CompleteSocket)
 local closeSocketListener = event.listen("GERTConnectionClose",CloseSocket)
 local GERTDataHandlerListener = event.listen("GERTData",GERTDataHandler)
 
-io.write("OpenFTP Beta 3 LITE has been successfully started.\nTo shut down the program, type EXIT.\nIf the io crashes, the program will continue running in the background.\nThe only way to stop the program after this is to either kill its processes, or reboot.\n")
+io.write("OpenFTP Beta 3 LITE has been successfully started.\nTo shut down the program, type EXIT, to run in background, type BACKGROUND.\nIf the io crashes, the program will continue running in the background.\nThe only way to stop the program after this is to either kill its processes, or reboot.\n")
 local response = true
-while response ~= "EXIT" do
+while response ~= "EXIT" and response ~= "BACKGROUND" do
     response = io.read()
+end
+if response == "BACKGROUND" then
+    os.exit()
 end
 event.cancel(completeSocketListener)
 event.cancel(closeSocketListener)
