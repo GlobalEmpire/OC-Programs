0f57a8d2ed51f782b9dca3f6c8204462fa9770ac
leothehero <leothehero@users.noreply.github.com>
2022-02-07 16:00:09 +0100

    Create OpenFTP Client Program LITE.lua

---
+++ Programs/OpenFTP/Beta/Beta 3/OpenFTP Client Program LITE.lua
@@ -0,0 +1,7 @@
+-- OpenFTP Client - Lite (Beta 3) | B3LITE
+
+local fs = require("filesystem")
+local GERTi = require("GERTiClient")
+local event = require("event")
+
+local FTPCore = require("FTPCore")
